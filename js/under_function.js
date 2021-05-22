900f33dc5d8724feae31122b10209814f8189f46
buntya <82546544+buntya@users.noreply.github.com>
2021-05-22 23:44:42 +0900

    Add files via upload

---
+++ js/under_function.js
@@ -0,0 +1,31 @@
+$(function(){
+  $('.js-modal-open').on('click',function(){
+      $('.js-modal').fadeIn();
+			$('.hamburger').addClass('hamburger__none')
+      return false;
+  });
+  $('.js-modal-close').on('click',function(){
+      $('.js-modal').fadeOut();
+			$('.hamburger').removeClass('hamburger__none')
+      return false;
+  });
+});
+
+//AOS.js
+AOS.init();
+
+
+//ハンバーガーメニュー
+$(function() {
+	$('.hamburger').click(function() {
+			$(this).toggleClass('active');
+
+			if ($(this).hasClass('active')) {
+					$('.globalMenuSp').addClass('active');
+					$('html').addClass('is-fixed');
+			} else {
+					$('.globalMenuSp').removeClass('active');
+					$('html').removeClass('is-fixed');
+			}
+	});
+});
